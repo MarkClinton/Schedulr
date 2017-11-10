a93fecba597ee4cbfaf408ff4b748df9a74de1cb
Mark Clinton <markc9309@yahoo.com>
2017-11-10 16:40:00 -0500

    First instance of calendar with events

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
 
     $('#calendar').fullCalendar({
-            header: { center: 'month,agendaWeek,basicDay,agendaDay' }, // buttons for switching between views
+            header: { 
+            	right: 'month,agendaWeek,agendaDay,listDay' ,
+            	center: 'title',
+    			left: 'prev,next'
+            }, 
 
+events: function(start, end, timezone, callback) {
+        $.ajax({
+            url: 'displayTasks',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                // our hypothetical feed requires UNIX timestamps
+                start: start.unix(),
+                end: end.unix()
+            },
+            success: function(response) {
+            	//var task = JSON.parse(response);
+            	console.log(JSON.stringify(response));
+            	console.log(response[0].TASK_NAME);
+                var events = [];
+                    events.push({
+                        title: response[0].TASK_NAME,
+                        start: response[0].TASK_DATE 
+                    });
+
+                callback(events);
+            }
+        });
+    }
 
     })
 
